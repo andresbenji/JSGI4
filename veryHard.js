41bb85066b1edb5ae64513acee16a3e5ffba213f
andresbenji <116680825+andresbenji@users.noreply.github.com>
2023-05-22 15:40:25 -0400

    very hard

--- veryHard.js
+++ veryHard.js
@@ -1,2 +1,14 @@
-const denominations = [1, 2, 5, 10];
-let total = 11;
+const coinChange = (coins, amount) => {
+  const dp = Array(amount + 1).fill(Infinity); // This arr tells us how many coins we need for each amount.
+  dp[0] = 0; // To make 0, we need 0 coins.
+  for (let coin of coins) {
+    // Check each coin
+    for (i = coin; i <= amount; i++) {
+      // Iterate through the entire amount from coin
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1); // Update minimum number of needed coins.
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount]; // If the last element is Infinity, then we cannot make the amount.
+};
+
+console.log(coinChange([1, 2, 5], 11));
